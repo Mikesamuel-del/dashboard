d6d885aad46a9de75ac089b558159ce67316b9de
king mike <mikesamuel387@gmail.com>
2026-05-14 13:28:47 +0300

    Update Packages.jsx

--- src/components/Packages.jsx
+++ src/components/Packages.jsx
@@ -4,6 +4,9 @@ import { toast } from "react-toastify";
 const API_BASE =
   process.env.REACT_APP_API_BASE ||
   "http://localhost:5000";
+  
+const userId =
+    authedUser?.id || authedUser?._id;
 
 const Packages = ({
   user,
